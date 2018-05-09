adc74947a2d91e943442998af938a7331d770be5
chris <christopheraburns@gmail.com>
2018-05-08 21:07:01 -0500

    Extended pause to prevent 503 error - included code to replace invalid characters in file names.

--- main.py
+++ main.py
@@ -11,26 +11,49 @@ import time
 import urllib
 import urllib2
 from bs4 import BeautifulSoup
-
 global dirName
+hitCounter = 0
 
 baseUrl = "https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/"
 
-#grass type was used for debugging - don't forget to add it back to the below collection
-energyTypes = ['?card-lightning=on', '?card-darkness=on', '?card-fairy=on', '?card-fire=on',
-               '?card-psychic=on', '?card-metal=on', '?card-dragon=on', '?card-water=on', '?card-fighting=on', '?card-colorless=on']
+#grass, lightning, darkness, fairy and fire types were used for debugging - don't forget to add it back to the below collection
+#energyTypes = ['?card-fire=on', '?card-psychic=on', '?card-metal=on', '?card-dragon=on', '?card-water=on', '?card-fighting=on', '?card-colorless=on']
+energyTypes = ['?card-psychic=on', '?card-metal=on', '?card-dragon=on', '?card-water=on', '?card-fighting=on', '?card-colorless=on']
 totPagesNum = 0
 
+# replace any invalid filename characters
+def nameCleaner(origFileName):
+    if origFileName.find('?') > -1:
+        print("? Character found in filename - replacing...")
+        origFileName = origFileName.replace('?', '_')
+
+    if origFileName.find('*') > -1:
+        print("* Character found in filename - replacing...")
+        origFileName = origFileName.replace('*', '_')
+
+    return origFileName
 
 
 def makesoup(currentUrl):
+    global hitCounter
+    if hitCounter == 9:
+        print("resetting connection... sleeping for 60 seconds")
+        time.sleep(60)
+        print("re-establishing connection...")
+        # Connect to a different site to reset the connection to Pokemon
+        response = urllib2.urlopen('https://www.google.com')
+        response.close()
+        hitCounter = 0
     print("Calling URL: {}".format(currentUrl))
+
     for attempt in range(1,3):
         try:
             response = urllib2.urlopen(currentUrl)
             soup = BeautifulSoup(response.read(), 'html.parser')
             time.sleep(1)
             response.close()
+            hitCounter = hitCounter + 1
+            print ('{} successful connections made...'.format(str(hitCounter)))
             return soup
         except Exception as e:
             if attempt == 0:
@@ -40,15 +63,10 @@ def makesoup(currentUrl):
                 print('Second connectoion attempt failed, trying again in 2 seconds')
                 time.sleep(2)
             elif attempt == 2:
-                print('Third connection attempt failed, trying again in 3 seconds')
+                print('Third connection attempt failed. Unable to connect to Pokemon site: {}'.format(e.message))
                 time.sleep(3)
-            else:
-                print("Error! {}{}".format("Unable to connect to Pokemon site:", e.message))
 
-
-
-
-def getAllByEnergyType(energyType = '', spec_range=[]):
+def getAllByEnergyType(energyType = '', spec_range=[1,99]):
     if energyType == '':
         exit()
     else:
@@ -93,6 +111,7 @@ def getAllByEnergyType(energyType = '', spec_range=[]):
                     imgUrl = strImage[strImage.index('src="') + 5: strImage.index('"/>')]
                     parts = strImage.split('_', 2)
                     origfileName = parts[2][0:parts[2].index('"')]
+                    origfileName = nameCleaner(origfileName)
                     newFileName = pokemonName + '_' + origfileName
 
                     if os.path.isfile('./images/' + dirName + '/' + newFileName) is not True:
@@ -102,13 +121,18 @@ def getAllByEnergyType(energyType = '', spec_range=[]):
             except ValueError as e:
                 print("ERROR! {}".format(e.message))
 
+hitCounter = 0
+
 for type in energyTypes:
-    #if type == '?card-grass=on':
-        #spec_range = [76,99]
-        #getAllByEnergyType('?card-grass=on', spec_range)
+
     parts = type.split('-')
     dirName = parts[1][0: parts[1].index('=')]
-
-    os.makedirs('./images/' + dirName)
-    getAllByEnergyType(type)
+    if os.path.isdir('./images/' + dirName) is not True:
+        os.makedirs('./images/' + dirName) # Create the directory of needed
+    print('Downloading {} Pokemon card images'.format(dirName))
+    if type == '?card-psychic=on':
+        spec_range = [52,99]
+        getAllByEnergyType('?card-psychic=on', spec_range)
+    else:
+        getAllByEnergyType(type)
 
